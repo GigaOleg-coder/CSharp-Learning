2b84e8a123ab978601b7b68b21d3d7f524e2b3bb
Oleg <yuzyukoleg6@gmail.com>
2026-04-22 22:43:35 +0300

    first project

---
+++ -basics/Lesson 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wake up, Neo ... ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("The Matrix has you ...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Follow the white rabbit.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
